61b6cd799069d9f86b1f357c98b53f5ed24ff59b
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2022-10-05 17:44:19 +0700

    20210140078

--- BubbleShortA/Program.cs
+++ BubbleShortA/Program.cs
@@ -4,9 +4,45 @@ namespace BubbleShortA
 {
     class Program
     {
-        static void Main(string[] args)
+        private int[] a = new int[20];
+        private int n;
+        public void read()
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Console.Write("Masukan banyaknya elemen pada Array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 20 elemen.\n");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("----------------------");
+            Console.WriteLine(" Masukan elemen array");
+            Console.WriteLine("----------------------");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                a[i] = Int32.Parse(s1);
+            }
         }
-    }
-}
+        public void display()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine(" Element array yang telah tersusun ");
+            Console.WriteLine("-----------------------------------");
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine(a[j]);
+            }
+            Console.WriteLine("");
+        }
+        public void BubbleSortArray()
+        {
+            for (int i = 1;)
+        }
